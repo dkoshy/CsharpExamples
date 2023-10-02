b7f2f0af1b64b3b0cc322d627301fb09f01eed30
dkoshy <deepakkoshy08@gmail.com>
2023-10-02 18:00:31 +0530

    functional examples.

---
+++ FunctinalExamples/Functinal.Examples/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Functinal.Examples.util;
+
+
+Console.WriteLine("Hello, World!");
+
+var time =
+    HelperMethods
+    .Using(() => new System.Net.WebClient(),
+      client => client.DownloadString("https://worldtimeapi.org/api/ip"));
+Console.WriteLine(time);
+
+
---
+++ FunctinalExamples/Functinal.Examples/util/HelperMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Functinal.Examples.util
+{
+    public static class HelperMethods
+    {
+        public static TResult Using<TDisposable,TResult>(
+            Func<TDisposable> factory,
+            Func<TDisposable,TResult> map) where TDisposable : IDisposable
+        {
+            using var tDisposable = factory();
+            return map(tDisposable);
+        }
+    }
+}
